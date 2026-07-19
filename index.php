86cec47597c04da0d84d7300945573c82ba81808
ctih1 <onni09nevala@gmail.com>
2026-07-19 23:42:52 +0300

    underscore not dot

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ function block_country($target_country) {
     
     $ip_file = lower_trimmed(file_get_contents("ips/".lower_trimmed($target_country).".txt"));
 
-    $added_lines = PHP.EOL."#c=".$target_country.";start";
+    $added_lines = PHP_EOL."#c=".$target_country.";start";
     $added_lines = $added_lines.PHP_EOL.$ip_file.PHP_EOL;
     $added_lines = $added_lines."#c=".$target_country.";end".PHP_EOL;
 
